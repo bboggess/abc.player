105be4bebd77f0f47251472b471bd924e5697f0a
bboggess <b.boggess727@gmail.com>
2024-02-04 11:35:32 -0600

    Test for required header fields
    Add tests that will check that the parser errors when
    required fields (title, track number, and key) are missing.

---
+++ abc.parser.antlr.test/RequiredFieldsHeaderTests.cs
@@ -0,0 +1,64 @@
+﻿namespace abc.parser.antlr.test;
+
+/// <summary>
+/// Tests that required header fields are in fact required.
+/// </summary>
+public partial class RequiredFieldsHeaderTests
+{
+    private const string HEADER_NO_TITLE = """
+                                           X:1
+                                           K:C
+                                           
+                                           """;
+
+    private const string HEADER_NO_KEY = """
+                                         X:1
+                                         T:Piece 1
+                                         
+                                         """;
+
+    private const string HEADER_NO_TRACK = """
+                                           T:Piece 1
+                                           K:C
+                                             
+                                           """;
+
+    [Test]
+    public void ParserErrorIfMissingTitle()
+    {
+        
+        var errorDetector = new ParserErrorDetector();
+        var parser = SetupHelpers.SetUpParser(HEADER_NO_TITLE, errorDetector);
+
+        // Now let's actually parse the header
+        var _ = parser.abcFile();
+
+        Assert.That(errorDetector.HasErrors, Is.True);
+    }
+
+    [Test]
+    public void ParserErrorIfMissingKey()
+    {
+
+        var errorDetector = new ParserErrorDetector();
+        var parser = SetupHelpers.SetUpParser(HEADER_NO_KEY, errorDetector);
+
+        // Now let's actually parse the header
+        var _ = parser.abcFile();
+
+        Assert.That(errorDetector.HasErrors, Is.True);
+    }
+
+    [Test]
+    public void ParserErrorIfMissingTrackNum()
+    {
+
+        var errorDetector = new ParserErrorDetector();
+        var parser = SetupHelpers.SetUpParser(HEADER_NO_TRACK, errorDetector);
+
+        // Now let's actually parse the header
+        var _ = parser.abcFile();
+
+        Assert.That(errorDetector.HasErrors, Is.True);
+    }
+}
--- abc.parser.antlr.test/SetupHelpers.cs
+++ abc.parser.antlr.test/SetupHelpers.cs
@@ -28,4 +28,30 @@ internal class SetupHelpers
 
         return parser.abcFile();
     }
+
+    /// <summary>
+    /// Builds up the parser from file content, allowing you to attach error listeners
+    /// to catch potential parse errors.
+    /// 
+    /// This does not actually attempt to create the parse tree, you must do that
+    /// manually.
+    /// </summary>
+    /// <param name="testContent">
+    /// String containing the ABC file content to test with. This needs to be
+    /// formatted exactly as an ABC file would be, with newlines and all.
+    /// </param>
+    /// <param name="errorListener">An error listener to attach. Will replace the default.</param>
+    /// <returns>A parser you can then use to build a parse tree</returns>
+    public static ABCParser SetUpParser(string testContent, IAntlrErrorListener<IToken> errorListener)
+    {
+        var inputStream = new AntlrInputStream(testContent);
+        var lexer = new ABCLexer(inputStream);
+        var tokens = new CommonTokenStream(lexer);
+
+        var parser = new ABCParser(tokens);
+        parser.RemoveErrorListeners();
+        parser.AddErrorListener(errorListener);
+
+        return parser;
+    }
 }
