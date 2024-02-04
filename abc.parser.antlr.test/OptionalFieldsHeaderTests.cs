1a03eebb58e8f461ef8114193a896ee462e0488e
bboggess <b.boggess727@gmail.com>
2024-02-04 11:41:44 -0600

    Test that optional fields can be omitted

---
+++ abc.parser.antlr.test/OptionalFieldsHeaderTests.cs
@@ -0,0 +1,26 @@
+﻿namespace abc.parser.antlr.test;
+
+/// <summary>
+/// Tests that optional fields can be omitted from the header.
+/// </summary>
+public class OptionalFieldsHeaderTests
+{
+    private const string NO_OPTIONAL_FIELDS_HEADER = """
+                                                    X:1
+                                                    T:Piece 1
+                                                    K:C
+
+                                                    """;
+
+    [Test]
+    public void NoParserErrorWhenNoOptionalFields()
+    {
+        var errorDetector = new ParserErrorDetector();
+        var parser = SetupHelpers.SetUpParser(NO_OPTIONAL_FIELDS_HEADER, errorDetector);
+
+        // Now let's actually parse the header
+        var _ = parser.abcFile();
+
+        Assert.That(errorDetector.HasErrors, Is.False);
+    }
+}
