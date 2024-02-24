9a0027a548223d17faef30a6b29255758c6b544d
bboggess <b.boggess727@gmail.com>
2024-02-24 13:42:36 -0600

    Get the symbol from token properly

--- abc.parser.antlr/visitor/FractionVisitor.cs
+++ abc.parser.antlr/visitor/FractionVisitor.cs
@@ -10,8 +10,8 @@ public class FractionVisitor : AbcHeaderBaseVisitor<Ratio>
 {
     public override Ratio VisitFraction([NotNull] AbcHeaderParser.FractionContext context)
     {
-        var numerator = int.Parse(context.INT(0).GetText());
-        var denominator = int.Parse(context.INT(1).GetText());
+        var numerator = int.Parse(context.INT(0).Symbol.Text);
+        var denominator = int.Parse(context.INT(1).Symbol.Text);
 
         return new Ratio(numerator, denominator);
     }
