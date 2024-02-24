f2ed8f01d8e9615983815cf449dcdc7ca601d19f
bboggess <b.boggess727@gmail.com>
2024-02-24 14:47:05 -0600

    Parse track title

---
+++ abc.parser.antlr.test/header/TrackTitleVisitorTests.cs
@@ -0,0 +1,42 @@
+﻿using abc.parser.antlr.visitor;
+using Antlr4.Runtime.Misc;
+
+namespace abc.parser.antlr.test.header;
+public class TrackTitleVisitorTests
+{
+    [Test]
+    public void ParseNonEmptyTitle()
+    {
+        var stringUnderTest = "T:Opus No. 2\n";
+        var parser = SetupHeaderHelpers.SetUpParser(stringUnderTest);
+        var visitor = new TrackTitleVisitor();
+
+        var outcome = visitor.Visit(parser.fieldTitle());
+
+        Assert.That(outcome, Is.EqualTo("Opus No. 2"));
+    }
+
+    [Test]
+    public void RejectsWhitespaceTitle()
+    {
+        var stringUnderTest = "T: \t  \n";
+        var parser = SetupHeaderHelpers.SetUpParser(stringUnderTest);
+        var visitor = new TrackTitleVisitor();
+
+        var action = () => visitor.Visit(parser.fieldTitle());
+
+        Assert.That(action, Throws.InstanceOf<ParseException>());
+    }
+
+    [Test]
+    public void RejectsEmptyTitle()
+    {
+        var stringUnderTest = "T:\n";
+        var parser = SetupHeaderHelpers.SetUpParser(stringUnderTest);
+        var visitor = new TrackTitleVisitor();
+
+        var action = () => visitor.Visit(parser.fieldTitle());
+
+        Assert.That(action, Throws.InstanceOf<ParseCanceledException>());
+    }
+}
---
+++ abc.parser.antlr/visitor/TrackTitleVisitor.cs
@@ -0,0 +1,14 @@
+﻿using Antlr4.Runtime.Misc;
+
+namespace abc.parser.antlr.visitor;
+
+/// <summary>
+/// Parses the title field of the track as a simple string
+/// </summary>
+public class TrackTitleVisitor : AbcHeaderBaseVisitor<string>
+{
+    public override string VisitFieldTitle([NotNull] AbcHeaderParser.FieldTitleContext context)
+    {
+        return new FreeTextVisitor().Visit(context.text());
+    }
+}
