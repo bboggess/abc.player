69632687930313cd61de675c14a961e72fc70ae1
bboggess <b.boggess727@gmail.com>
2024-02-24 14:59:31 -0600

    Parse base note length field

---
+++ abc.parser.antlr.test/header/BaseNoteLengthVisitorTests.cs
@@ -0,0 +1,57 @@
+﻿using abc.parser.antlr.model;
+using abc.parser.antlr.visitor;
+using Antlr4.Runtime.Misc;
+
+namespace abc.parser.antlr.test.header;
+
+public class BaseNoteLengthVisitorTests
+{
+    [Test]
+    public void AcceptsValidRatios([Random(3)] int top, [Random(3)] int bottom)
+    {
+        var stringUnderTest = $"L:{top}/{bottom}\n";
+        var parser = SetupHeaderHelpers.SetUpParser(stringUnderTest);
+        var visitor = new BaseNoteLengthVisitor();
+
+        var outcome = visitor.Visit(parser.fieldLength());
+
+        var expected = new Ratio(top, bottom);
+        Assert.That(outcome, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void RejectsMissingNumerator([Random(3)] int bottom)
+    {
+        var stringUnderTest = $"L:/{bottom}\n";
+        var parser = SetupHeaderHelpers.SetUpParser(stringUnderTest);
+        var visitor = new BaseNoteLengthVisitor();
+
+        var action = () => visitor.Visit(parser.fieldLength());
+
+        Assert.That(action, Throws.InstanceOf<ParseCanceledException>());
+    }
+
+    [Test]
+    public void RejectsMissingDenominator([Random(3)] int top)
+    {
+        var stringUnderTest = $"L:{top}/\n";
+        var parser = SetupHeaderHelpers.SetUpParser(stringUnderTest);
+        var visitor = new BaseNoteLengthVisitor();
+
+        var action = () => visitor.Visit(parser.fieldLength());
+
+        Assert.That(action, Throws.InstanceOf<ParseCanceledException>());
+    }
+
+    [Test]
+    public void RejectsNonFraction([Random(3)] int num)
+    {
+        var stringUnderTest = $"L:{num}\n";
+        var parser = SetupHeaderHelpers.SetUpParser(stringUnderTest);
+        var visitor = new BaseNoteLengthVisitor();
+
+        var action = () => visitor.Visit(parser.fieldLength());
+
+        Assert.That(action, Throws.InstanceOf<ParseCanceledException>());
+    }
+}
---
+++ abc.parser.antlr/visitor/BaseNoteLengthVisitor.cs
@@ -0,0 +1,15 @@
+﻿using abc.parser.antlr.model;
+using Antlr4.Runtime.Misc;
+
+namespace abc.parser.antlr.visitor;
+
+/// <summary>
+/// Parsing the note length to use as the base for all notes in the body
+/// </summary>
+public class BaseNoteLengthVisitor : AbcHeaderBaseVisitor<Ratio>
+{
+    public override Ratio VisitFieldLength([NotNull] AbcHeaderParser.FieldLengthContext context)
+    {
+        return new FractionVisitor().Visit(context.fraction()) ?? throw new ParseException(context);
+    }
+}
