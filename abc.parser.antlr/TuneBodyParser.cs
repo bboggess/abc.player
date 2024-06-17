3ffd7c8d192463a01738a508e22fad703cead1d9
bboggess <b.boggess727@gmail.com>
2024-06-16 19:48:27 -0500

    Antlr body parsing implementation
    Final piece of the implementation of body parsing
    with Antlr.

---
+++ abc.parser.antlr/TuneBodyParser.cs
@@ -0,0 +1,37 @@
+﻿using abc.parser.parse;
+using Antlr4.Runtime.Tree;
+
+namespace abc.parser.antlr;
+
+/// <summary>
+/// Parses the body of an ABC tune. Walks through the entire body, sending
+/// messages to the domain layer as discrete musical entities are parsed.
+/// </summary>
+public class TuneBodyParser : ITuneProcessor
+{
+    private readonly AbcContextProvider _provider;
+
+    /// <summary>
+    /// Sets up a parser with the necessary Antlr context
+    /// </summary>
+    /// <param name="provider">Provides the context of an Antlr parse tree</param>
+    public TuneBodyParser(AbcContextProvider provider)
+    {
+        if (provider is null)
+        {
+            throw new ArgumentNullException(nameof(provider));
+        }
+
+        _provider = provider;
+    }
+
+    public void Process(ITuneBodyParser parser)
+    {
+        var bodyContext = _provider.GetBodyContext();
+
+        var walker = new ParseTreeWalker();
+        var listener = new TuneBodyListener(parser);
+
+        walker.Walk(listener, bodyContext);
+    }
+}
---
+++ abc.parser/ITuneProcessor.cs
@@ -0,0 +1,16 @@
+﻿using abc.parser.parse;
+
+namespace abc.parser;
+
+/// <summary>
+/// An object capable of walking through and processing a specific tune.
+/// </summary>
+public interface ITuneProcessor
+{
+    /// <summary>
+    /// Processes the full tune body, using the provided parser
+    /// to parse the raw data being processed.
+    /// </summary>
+    /// <param name="parser">Takes in unstructured events and parses them.</param>
+    void Process(ITuneBodyParser parser);
+}
